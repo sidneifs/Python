9202f4b2fbebee20f50c037259740e13ec238fd4
sidneifs <supersidnei@gmail.com>
2025-03-16 17:34:38 -0300

    Atualizações de Pasta. Adicionado Python_03 e Python_04

---
+++ Python_04/variaveis_e_constantes.py
@@ -0,0 +1,21 @@
+age = 37
+name = 'Sidnei'
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
+
+age, name = (37, 'Sidnei')
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
+
+limite_saque_diario = 1000
+
+stateS = 'MG'
+stateS = 'SP'   # Não é uma constante, pois pode ser alterada
+BRASILIAN_STATES = ('MG', 'SP', 'RJ', 'ES')  # Tupla é uma constante
+
+PI = 3.14159265359
+print(PI)
+PI = 3.14  # Não é uma constante, pois pode ser alterada
+print(PI)   # Não é uma constante, pois pode ser alterada
+
+print(BRASILIAN_STATES)
+print(stateS)
+print(limite_saque_diario)
